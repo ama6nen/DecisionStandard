6d8193b8c06ca8e45ff28e97ab13b262c00b75c2
Ama <ama6nen@gmail.com>
2020-05-19 09:15:45 +0300

    Basic viewmodel structures

--- DecisionStandard/ViewModels/ConfigViewModel.cs
+++ DecisionStandard/ViewModels/ConfigViewModel.cs
@@ -3,9 +3,28 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
+using DecisionStandard.Models;
+using System.Runtime.CompilerServices;
+
 namespace DecisionStandard.ViewModels
 {
     public class ConfigViewModel : Screen
     {
+        private readonly IWindowManager _windowMgr; //might be needed later
+        private ConfigModel config = null;
+
+        private void NotifyChange<T>(ref T field, T newval, [CallerMemberName] string name = "") //so we can actually use 1 liner for setter
+        {
+            field = newval;
+            NotifyOfPropertyChange(name);
+        }
+
+        public ConfigViewModel(IWindowManager manager)
+        {
+            _windowMgr = manager;
+            config = new ConfigModel();
+        }
+
+        public ConfigModel Config { get => config; set => NotifyChange(ref config, value); }
     }
 }
--- DecisionStandard/ViewModels/DecisionViewModel.cs
+++ DecisionStandard/ViewModels/DecisionViewModel.cs
@@ -2,10 +2,31 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using DecisionStandard.Models;
+using System.Runtime.CompilerServices;
+using DecisionStandard.Views;
 
 namespace DecisionStandard.ViewModels
 {
     public class DecisionViewModel : Screen
     {
+        private readonly IWindowManager _windowMgr; //might be needed later
+        private DecisionModel decision = null;
+
+        private void NotifyChange<T>(ref T field, T newval, [CallerMemberName] string name = "") //so we can actually use 1 liner for setter
+        {
+            field = newval;
+            NotifyOfPropertyChange(name);
+        }
+
+        public DecisionViewModel(IWindowManager manager)
+        {
+            var config = IoC.Get<ConfigView>(); //we will want to use config if its loaded with data to aid our standard
+            _windowMgr = manager;
+            decision = new DecisionModel();
+        }
+
+        public DecisionModel Config { get => decision; set => NotifyChange(ref decision, value); }
+
     }
 }
