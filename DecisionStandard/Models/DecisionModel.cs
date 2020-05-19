d2a23d0ac788cbde5aab92bb5a2c424e181544ac
Ama <ama6nen@gmail.com>
2020-05-19 10:55:11 +0300

    use model like ur supposed to in mvvm

--- DecisionStandard/Models/DecisionModel.cs
+++ DecisionStandard/Models/DecisionModel.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 
 namespace DecisionStandard.Models
 {
     public class DecisionModel
     {
+        public string Name { get; set; }
+       
+        //not needed or used for now, might be useful for identifying theselater though
+        private  static int IDCounter = 0;
+        private int ID = 0;
+        public DecisionModel()
+        {
+            ID = IDCounter++;
+        }
+
+        public void DebugPrint()
+        {
+            Debug.WriteLine("Name: " + Name);
+        }
     }
 }
--- DecisionStandard/ViewModels/DecisionViewModel.cs
+++ DecisionStandard/ViewModels/DecisionViewModel.cs
@@ -13,7 +13,8 @@ namespace DecisionStandard.ViewModels
     public class DecisionViewModel : Conductor<object>
     {
         private readonly IWindowManager _windowMgr; //might be needed later
-        private DecisionModel decision = null;
+        private DecisionModel firstDecision = null;
+        private DecisionModel secondDecision = null;
         private ConfigViewModel config = null;
 
         private void NotifyChange<T>(ref T field, T newval, [CallerMemberName] string name = "") //so we can actually use 1 liner for setter
@@ -26,20 +27,22 @@ namespace DecisionStandard.ViewModels
         {
             config = IoC.Get<ConfigViewModel>(); //we will want to use config if its loaded with data to aid our standard
             _windowMgr = manager;
-            decision = new DecisionModel();
+            firstDecision = new DecisionModel();
+            secondDecision = new DecisionModel();
         }
 
-        public DecisionModel Decision { get => decision; set => NotifyChange(ref decision, value); }
-       
-        //Realistically we dont need these in the Model itself
-        private string _firstDecision;
-        public string FirstDecision { get => _firstDecision; set => NotifyChange(ref _firstDecision, value); }
-        private string _secondDecision;
-        public string SecondDecision { get => _secondDecision; set => NotifyChange(ref _secondDecision, value); }
+        public DecisionModel FirstDecision { get => firstDecision; set => NotifyChange(ref firstDecision, value); }
+        public DecisionModel SecondDecision { get => secondDecision; set => NotifyChange(ref secondDecision, value); }
 
         public async void LoadConfigView()
         {
             await _windowMgr.ShowWindowAsync(IoC.Get<ConfigViewModel>());
+        }
+
+        public void DebugButton()
+        {
+            FirstDecision.DebugPrint();
+            SecondDecision.DebugPrint();
         }
     }
 }
